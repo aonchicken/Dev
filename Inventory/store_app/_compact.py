9f06f35ce1d7668e81d1ceb8368f6ec85186093d
aonchicken <aonchicken@gmail.com>
2018-07-16 15:26:18 +0700

    Revert "Initial commit"
    This reverts commit 6559477a7d3e19a3351e17c3e4866426b533d772.

--- Inventory/store_app/_compact.py
+++
@@ -1,9 +0,0 @@
-try:
-    from django.http import JsonResponse
-except ImportError:
-    from django.http import HttpResponse
-    import json
-
-    def JsonResponse(data):
-        return HttpResponse(json.dumps(data),
-                            content_type="application/json")
