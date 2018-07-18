cdc2f205a9a137d01246fc83e7f9201a0a00bc4e
aonchicken <aonchicken@gmail.com>
2018-07-18 14:07:39 +0700

    pdf & excel

--- Inventory/store_app/models.py
+++ Inventory/store_app/models.py
@@ -6,7 +6,10 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
+# TRUNCATE TABLE dbtest.store_app_choice;
+# SET FOREIGN_KEY_CHECKS = 0;
+# TRUNCATE TABLE dbtest.store_app_question;
+# SET FOREIGN_KEY_CHECKS = 1;
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
